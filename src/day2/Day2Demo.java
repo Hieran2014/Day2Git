f509d043aec8d4733524c09d1b83b9bc3462e619
GYTumlisan <gymoa2011@gmail.com>
2023-04-07 15:53:01 -0700

    Initial Commit

---
+++ src/day2/Day2Demo.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Day2Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("day2 demo local repository....");
+		
+		System.out.println("day2 demo Remote repository....");
+	}
+
+}
